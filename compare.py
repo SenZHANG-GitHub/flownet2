4591e34c57a68b391b00165f5c934c36cb2c0999
sen <zhangsen@dm-ai.cn>
2020-03-27 16:06:22 +1100

    add --dataset euroc_dataset

--- compare.py
+++ compare.py
@@ -47,7 +47,7 @@ def compare_flow_kitti(dataset, out_dir):
     (1) dataset:    kitti_odometry, kitti_2012, kitti_2015
     (2) out_dir: 'kitti_odometry/results/compare'
     """
-    use_gt = False if dataset == 'kitti_odometry' else True
+    use_gt = False if dataset in 'kitti_odometry' else True
     model_list = ['FlowNet2', 'FlowNet2C', 'FlowNet2S']
     num_model = len(model_list)
     if use_gt: num_model += 1 # include ground-truth for display
@@ -79,6 +79,43 @@ def compare_flow_kitti(dataset, out_dir):
             plt.savefig('{}/{}/{}'.format(out_dir, subtype, img_file))
 
 
+def compare_flow_euroc(dataset, out_dir):
+    """
+    (1) dataset:    euroc_odometry
+    (2) out_dir: 'euroc_odometry/compare'
+    """
+    use_gt = False # no gt for euroc dataset 
+    model_list = ['FlowNet2', 'FlowNet2C', 'FlowNet2S']
+    num_model = len(model_list)
+    if use_gt: num_model += 1 # include ground-truth for display
+    subtype_list = ['MH_04_difficult', 'MH_02_easy']
+    for subtype in subtype_list:
+        print('processing {}...'.format(subtype))
+        os.makedirs('{}/{}/00'.format(out_dir, subtype))
+        # get image filenames
+        img_list = [x for x in os.listdir('{}/FlowNet2/{}/00/inference/run.epoch-0-flow-vis'.format(dataset, subtype)) if x[-4:] == '.png']
+        for img_file in tqdm(img_list):
+            if use_gt:
+                raise NotImplementedError('to implement')
+                # img_subname = int(img_file.split('-')[0]) + 1
+                # img_path = 'Sintel/results/flow_viz/{}/frame_{:04d}.png'.format(subtype, img_subname)
+                tmp_img = img.imread(img_path)
+                plt.subplot(num_model, 1, 1)
+                plt.imshow(tmp_img)
+                plt.title('Ground Truth', y=-0.23) # plt.text(15, -0.01, "tmp")
+                plt.axis('off')
+            for i_model, flow_model in enumerate(model_list):
+                img_path = '{}/{}/{}/00/inference/run.epoch-0-flow-vis/{}'.format(dataset, flow_model, subtype, img_file)
+                tmp_img = img.imread(img_path)
+                sub_ind = i_model + 1 
+                if use_gt: sub_ind += 1 
+                plt.subplot(num_model, 1, sub_ind)
+                plt.imshow(tmp_img)
+                plt.title('{}'.format(flow_model), y=-0.23) 
+                plt.axis('off')
+            plt.savefig('{}/{}/00/{}'.format(out_dir, subtype, img_file))
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
@@ -100,6 +137,14 @@ if __name__ == "__main__":
         if os.path.isdir(out_dir): rmtree(out_dir)
         os.mkdir(out_dir)
         compare_flow_kitti(
+            dataset = args.dataset,
+            out_dir = out_dir
+        )
+    elif args.dataset == 'euroc_odometry':
+        out_dir = 'euroc_odometry/compare'
+        if os.path.isdir(out_dir): rmtree(out_dir)
+        os.mkdir(out_dir)
+        compare_flow_euroc(
             dataset = args.dataset,
             out_dir = out_dir
         )
